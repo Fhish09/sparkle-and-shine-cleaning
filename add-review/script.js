d0e3f6fac023189f8a83f69434ce01c4ef624b79
Nduonige Courage <nduonigec@gmail.com>
2026-02-15 15:49:29 +0100

    faaaah

--- add-review/script.js
+++ add-review/script.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("menu-toggle");
   const nav = document.getElementById("nav-links");
 
-  toggle.addEventListener("click", function () {
-    nav.classList.toggle("active");
-  });
+  if (toggle && nav) {
+    toggle.addEventListener("click", function () {
+      nav.classList.toggle("active");
+    });
+  }
 });
